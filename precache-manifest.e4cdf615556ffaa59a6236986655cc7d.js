48c7d24efbc16cce1d26658863c55f905ab957f7
Adie Christopher <adiec7@gmail.com>
2018-12-03 09:04:13 +0100

    Updates

---
+++ precache-manifest.e4cdf615556ffaa59a6236986655cc7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f4901e821191eba80e0",
+    "url": "/reactapp/static/css/main.9f4cffd3.chunk.css"
+  },
+  {
+    "revision": "9f4901e821191eba80e0",
+    "url": "/reactapp/static/js/main.9f4901e8.chunk.js"
+  },
+  {
+    "revision": "1293aac31d95dd8eaeaa",
+    "url": "/reactapp/static/css/1.ca6174a3.chunk.css"
+  },
+  {
+    "revision": "1293aac31d95dd8eaeaa",
+    "url": "/reactapp/static/js/1.1293aac3.chunk.js"
+  },
+  {
+    "revision": "75024734ab9c32c58e65",
+    "url": "/reactapp/static/js/runtime~main.75024734.js"
+  },
+  {
+    "revision": "4fd788f9b9da5bb57bc17a8a2d4261dc",
+    "url": "/reactapp/index.html"
+  }
+];
